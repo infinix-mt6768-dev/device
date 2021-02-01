b645398f90fb61cb424f82db0df227135d80863d
SparXFusion <s2234nadar@gmail.com>
2021-02-01 14:59:59 +0530

    X693: add in build fingerprint
    Signed-off-by: SparXFusion <s2234nadar@gmail.com>

--- lineage_X693.mk
+++ lineage_X693.mk
@@ -31,3 +31,11 @@ PRODUCT_NAME := lineage_X693
 PRODUCT_BRAND := Infinix
 PRODUCT_MODEL := Infinix NOTE 10
 PRODUCT_MANUFACTURER := infinix
+
+PRODUCT_GMS_CLIENTID_BASE := android-infinix
+TARGET_VENDOR := infinix
+TARGET_VENDOR_PRODUCT_NAME := X693
+PRODUCT_BUILD_PROP_OVERRIDES += PRIVATE_BUILD_DESC="sys_tssi_64_infinix-user-11-RP1A.200720.011-255379-release-keys"
+
+# Set BUILD_FINGERPRINT variable to be picked up by both system and vendor build.prop
+BUILD_FINGERPRINT := Infinix/X693-GL/Infinix-X693:11/RP1A.200720.011/230224V656:user/release-keys
